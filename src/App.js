a9706462a2adda40d6dc6efbd2a85fcd017b2036
EvelynCa <evelyn_castro96@hotmail.com>
2025-04-28 19:11:18 -0700

    frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import Lista from "./Lista";
+import Registro from "./Registro";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
